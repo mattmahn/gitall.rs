2ecd3fbefce58517a32819a20221e3950b06baf2
Matthew Mahnke <mmahnke18@gmail.com>
2021-02-12 08:59:51 -0600

    feat: add library entrypoint
    gitall can now be used as a crate for use by other Rust programs.

---
+++ src/lib.rs
@@ -0,0 +1,181 @@
+/*!
+Crate `gitall` provides an simple API for recursively finding all Git
+directories below a given directory.
+
+# Example
+
+The following code prints the directory paths of each Git directory located
+beneath the current directory:
+```
+use gitall::Giterator;
+use std::default::Default;
+
+let giterator = Giterator::default();
+for direntry in giterator {
+    println!("{}", direntry.path().display());
+}
+```
+
+This snippet only finds Git directories whose name matches a regular expression:
+```
+use gitall::Giterator;
+use regex::Regex;
+use std::default::Default;
+
+let giterator = Giterator::default().regex(Regex::new(r"some-pattern-.+").unwrap());
+for direntry in giterator {
+    println!("{}", direntry.path().display());
+}
+```
+
+Check the [`Giterator`][] documentation for more ways to filter results.
+*/
+
+use regex::Regex;
+use walkdir::{DirEntry, WalkDir};
+
+use std::{
+    default::Default,
+    iter,
+    path::{Path, PathBuf},
+};
+
+#[derive(Debug)]
+struct GiteratorOptions {
+    root: PathBuf,
+    follow_links: bool,
+    max_depth: usize,
+    regex: Regex,
+    match_full_path: bool,
+}
+
+/// A builder to define how to search for Git directories.
+#[derive(Debug)]
+pub struct Giterator {
+    options: GiteratorOptions,
+}
+
+impl Giterator {
+    /// Sets the apex root directory to begin searching. The directory itself
+    /// will not be considered for inclusion in the final results; only
+    /// directories within `root`.
+    pub fn root<P: AsRef<Path>>(mut self, root: P) -> Self {
+        self.options.root = root.as_ref().to_path_buf();
+        self
+    }
+
+    /// Sets wether or not to follow symbolic links in the directory tree.
+    pub fn follow_links(mut self, follow: bool) -> Self {
+        self.options.follow_links = follow;
+        self
+    }
+
+    /// Sets the maximum number of directories from `root` to search. Directories
+    /// more than `depth` directories deeper than `root` will not be found.
+    pub fn max_depth(mut self, depth: usize) -> Self {
+        self.options.max_depth = depth;
+        self
+    }
+
+    /// Sets the regular expression that must match directory paths.
+    pub fn regex(mut self, regex: Regex) -> Self {
+        self.options.regex = regex;
+        self
+    }
+
+    /// Sets wether or not to match `regex` against the full
+    /// [canonicalized][canonical] directory path.
+    ///
+    /// [canonical]: std::path::Path::canonicalize
+    pub fn match_full_path(mut self, match_full_path: bool) -> Self {
+        self.options.match_full_path = match_full_path;
+        self
+    }
+}
+
+impl Default for Giterator {
+    /// Creates a default Giterator with the following settings:
+    ///   - search the current working directory
+    ///   - do not follow symbolic links
+    ///   - search [`usize::MAX`][] directories deep
+    ///   - match any directory path
+    fn default() -> Self {
+        Giterator {
+            options: GiteratorOptions {
+                root: Path::new(".").to_path_buf(),
+                follow_links: false,
+                max_depth: usize::MAX,
+                regex: Regex::new(".*").unwrap(),
+                match_full_path: false,
+            },
+        }
+    }
+}
+
+impl iter::IntoIterator for Giterator {
+    type Item = DirEntry;
+    type IntoIter = IntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        IntoIter {
+            walker: WalkDir::new(self.options.root).into_iter(),
+            regex: self.options.regex,
+            full_path: self.options.match_full_path,
+        }
+    }
+}
+
+#[derive(Debug)]
+/// An iterator for recursively finding Git directories.
+pub struct IntoIter {
+    walker: ::walkdir::IntoIter,
+    regex: Regex,
+    full_path: bool,
+}
+
+impl iter::Iterator for IntoIter {
+    type Item = DirEntry;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let entry = match self.walker.next() {
+                None => return None, // no more directories to walk
+                Some(Err(_err)) => {
+                    // TODO provide a debug log message
+                    self.walker.skip_current_dir();
+                    continue;
+                }
+                Some(Ok(entry)) => entry,
+            };
+            if is_git_dir(&entry) {
+                self.walker.skip_current_dir();
+                if is_matching_dir(&entry, &self.regex, self.full_path) {
+                    return Some(entry);
+                }
+            }
+        }
+    }
+}
+
+/// Returns true if the directory represented by `entry` is a Git directory root.
+pub fn is_git_dir(entry: &DirEntry) -> bool {
+    entry.file_type().is_dir() && entry.path().join(".git").is_dir()
+}
+
+/// Returns true if the directory represented by `entry` matches the given
+/// regular express. If `full_path` is true, the regex matches against the
+/// [canonicalized][canonical] path of `entry`.
+///
+/// [canonical]: std::path::Path::canonicalize
+fn is_matching_dir(entry: &DirEntry, regex: &Regex, full_path: bool) -> bool {
+    let canonical_path = entry
+        .path()
+        .canonicalize()
+        .expect("failed to canonicalize the directory path");
+    let asdf: &str = if full_path {
+        canonical_path.to_str().unwrap()
+    } else {
+        entry.file_name().to_str().unwrap()
+    };
+    regex.is_match(asdf)
+}
--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,34 @@ use atty;
 use clap::ArgMatches;
 use rayon;
 use regex::Regex;
-use termcolor::{StandardStream};
-use walkdir::{DirEntry, WalkDir};
+use termcolor::StandardStream;
 
+use std::default::Default;
 use std::ffi::OsStr;
-use std::process::{Command, Output};
 use std::path::PathBuf;
-use std::sync::Arc;
+use std::process::{Command, Output};
 use std::sync::mpsc::channel;
-use std::iter::{IntoIterator, Iterator};
+use std::sync::Arc;
 
 mod cli;
+mod lib;
+use lib::Giterator;
 mod output;
 
 /// List of Git commands that support the `--color` option.  Only commands shown
 /// here will have `--color` set in the spawned process.
-const COMMANDS: &[&'static str] = &["branch", "diff", "diff-index",
-    "format-patch", "grep", "log", "reflog", "rev-list", "shortlog", "show"];
+const COMMANDS: &[&'static str] = &[
+    "branch",
+    "diff",
+    "diff-index",
+    "format-patch",
+    "grep",
+    "log",
+    "reflog",
+    "rev-list",
+    "shortlog",
+    "show",
+];
 
 #[derive(Debug)]
 pub struct GitResult {
@@ -38,56 +49,45 @@ fn main() {
 
     // configure directory tree walker
     let parent_dir = matches.value_of_os("dir").unwrap_or(OsStr::new("."));
-    let mut wd = WalkDir::new(parent_dir)
-        .min_depth(1)
+    let mut g = Giterator::default()
+        .root(parent_dir)
         .follow_links(matches.is_present("follow_links"));
+
     if let Some(max_depth) = matches.value_of("max_depth") {
-        wd = wd.max_depth(usize::from_str_radix(max_depth, 10).expect("max-depth option could not be parsed as a base-10 number"));
+        g = g.max_depth(
+            usize::from_str_radix(max_depth, 10)
+                .expect("max-depth option could not be parsed as a base-10 number"),
+        );
     }
 
-    let regex_str = matches.value_of("regex").unwrap();  // default is provided to clap; can safely unwrap
+    let regex_str = matches.value_of("regex").unwrap(); // default is provided to clap; can safely unwrap
     let regex = Regex::new(regex_str).expect("regex option is not a valid regular expression (see https://docs.rs/regex/1/regex/#syntax for syntax)");
+    g = g.regex(regex);
+
     let regex_full_path = matches.is_present("full_path");
+    g = g.match_full_path(regex_full_path);
 
     // walk directory tree
-    let mut it = wd.into_iter();
-    loop {
-        let entry = match it.next() {
-            None => break,
-            Some(Err(err)) => {
-                eprintln!("skipping directory due to error: {}", err);
-                it.skip_current_dir();
-                continue;
-            },
-            Some(Ok(entry)) => entry,
-        };
-        if is_git_dir(&entry) { // we've found a Git repo root
-            // stop searching below this directory since we've already
-            // found a Git repo root
-            it.skip_current_dir();
-            if is_matching_dir(&entry, &regex, regex_full_path) { // Only run commands for filtered repo(s)
-                let tx = tx.clone();
-                let args = git_command.clone();
-                let color_arg = git_color_arg.clone();
+    for entry in g {
+        // Only run commands for filtered repo(s)
+        let tx = tx.clone();
+        let args = git_command.clone();
+        let color_arg = git_color_arg.clone();
 
-                pool.spawn(move || {
-                    let mut command = Command::new("git");
-                    command.current_dir(entry.path())
-                        .args(args.as_ref());
-                    if should_set_color_arg(args.as_ref()) {
-                        command.arg(color_arg.as_ref());
-                    }
-                    let output = command.output()
-                        .expect("failed to execute git command");
-
-                    tx.send(GitResult {
-                        directory_name: entry.into_path(),
-                        output
-                    }).unwrap();
-                });
+        pool.spawn(move || {
+            let mut command = Command::new("git");
+            command.current_dir(entry.path()).args(args.as_ref());
+            if should_set_color_arg(args.as_ref()) {
+                command.arg(color_arg.as_ref());
             }
-        }
-        // here, `entry` _may not_ be a Git repo
+            let output = command.output().expect("failed to execute git command");
+
+            tx.send(GitResult {
+                directory_name: entry.into_path(),
+                output,
+            })
+            .unwrap();
+        });
     }
 
     // manually drop tx so the receiver ends
@@ -104,21 +104,6 @@ fn main() {
             output::print_git_result(&mut stdout_l, &mut stderr_l, gr).unwrap();
         }
     }
-}
-
-fn is_git_dir(entry: &DirEntry) -> bool {
-    entry.file_type().is_dir()
-        && entry.path().join(".git").is_dir()
-}
-
-fn is_matching_dir(entry: &DirEntry, regex: &Regex, full_path: bool) -> bool {
-    let canonical_path = entry.path().canonicalize().expect("failed to canonicalize the directory path");
-    let asdf: &str = if full_path {
-        canonical_path.to_str().unwrap()
-    } else {
-        entry.file_name().to_str().unwrap()
-    };
-    regex.is_match(asdf)
 }
 
 fn should_set_color_arg(cmd: &Vec<String>) -> bool {
